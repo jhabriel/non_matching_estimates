d53fad81a71a2d858bf73c110b21035c35fcb291
Jhabriel Varela <jhabriel@gmail.com>
2024-12-09 11:12:48 -0300

    Update __init__.py

--- src/mdnme/__init__.py
+++ src/mdnme/__init__.py
@@ -1,23 +1,23 @@
 # Error estimation
-from mdamr.estimates.diffusive_error import compute_diffusive_error
-from mdamr.estimates.residual_error import compute_residual_error
-from mdamr.estimates.error_estimation import (
+from mdnme.estimates.diffusive_error import compute_diffusive_error
+from mdnme.estimates.residual_error import compute_residual_error
+from mdnme.estimates.error_estimation import (
     compute_error_indicators,
     estimate_errors,
     get_majorant,
 )
 
-from mdamr.estimates.flux_extension import extend_fv_fluxes
-from mdamr.estimates.pressure_reconstruction import reconstruct_pressure
-from mdamr.estimates.helpers import ErrorEstimatesSaveData
+from mdnme.estimates.flux_extension import extend_fv_fluxes
+from mdnme.estimates.pressure_reconstruction import reconstruct_pressure
+from mdnme.estimates.helpers import ErrorEstimatesSaveData
 
 # Adaptive mesh refinement
-from mdamr.amr.nvb import refine_nvb
-from mdamr.amr.refine_2d import refine_rgb
-from mdamr.amr.rg import refine_rg
-from mdamr.amr.refine_1d import refine_red_1d
-from mdamr.amr.marking import doerfler_marking
+from mdnme.amr.nvb import refine_nvb
+from mdnme.amr.refine_2d import refine_rgb
+from mdnme.amr.rg import refine_rg
+from mdnme.amr.refine_1d import refine_red_1d
+from mdnme.amr.marking import doerfler_marking
 
 # Utilities
-from mdamr import utils
-from mdamr.utils.grid_rotation import RotatedGrid
+from mdnme import utils
+from mdnme.utils.grid_rotation import RotatedGrid
