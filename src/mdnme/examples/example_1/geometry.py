a9fd69efda316a068f7ad1979504d3884eea1f59
Jhabriel Varela <jhabriel@gmail.com>
2024-12-11 22:44:38 -0300

    ENH: Allow for non-matching grid creation

--- src/mdnme/examples/example_1/geometry.py
+++ src/mdnme/examples/example_1/geometry.py
@@ -62,3 +62,77 @@ class Varela2023JNumGeometry:
         if self.grid_type() == "simplex":
             kw_args = {"constraints": np.array([1, 2])}
         return kw_args
+
+    def set_geometry(self) -> None:
+
+        # Create the geometry through domain and fracture set.
+        self.set_domain()
+        self.set_fractures()
+
+        # Create a fracture network.
+        self.fracture_network = pp.create_fracture_network(
+            self.fractures,
+            self.domain
+        )
+
+        # Generate the mixed-dimensional grid
+
+        # If `non_matching_cell_sizes` is not given, then the mdg is matching, and
+        # we create it in the usual way
+        if self.params.get("non_matching_cell_sizes") is None:
+
+            mdg_final = pp.create_mdg(
+                self.grid_type(),
+                self.meshing_arguments(),
+                self.fracture_network,
+                **self.meshing_kwargs(),
+            )
+
+        # If `non_matching_cell_sizes` is given, we create the non-matching mdg
+        else:
+
+            # Retrieve the non-matching cell sizes. This is 3-tuple of floats. The
+            # first element is the target cell size for the matrix, the second
+            # element is the target cell size for the interface grid, and the third
+            # element is the target cell size for the fracture grid
+            cell_sizes: tuple[float, float, float] = self.params[
+                "non_matching_cell_sizes"
+            ]
+            assert len(cell_sizes) == 3
+
+            # The idea is to create three different fully matching mdgs and then
+            # retrieve the fracture and interface grids from these mdgs and replace
+            # into the mdg containing the "correct" higher-dimensional subdomain
+            # This is quite a lazy solution, but it works.
+            mdgs: list[pp.MixedDimensionalGrid] = []
+            for cell_size in cell_sizes:
+                mdg = pp.create_mdg(
+                    self.grid_type(),
+                    {"cell_size": cell_size},
+                    self.fracture_network,
+                    **self.meshing_kwargs(),
+                )
+                mdgs.append(mdg)
+
+            # Now we replace the grids and produce the non-matching mixed-dimensional
+            # grid. We keep the matrix fixed, and replace the fractures and mortar grids
+            mdg_final = mdgs[0]
+            mdg_final.replace_subdomains_and_interfaces(
+                sd_map={mdg_final.subdomains()[1]: mdgs[2].subdomains()[1]},
+                intf_map={mdg_final.interfaces()[0]: mdgs[1].interfaces()[0]},
+            )
+
+            # Make sure the geometry of the replaced boundary grids are computed.
+            # TODO: This has to be taken care by PorePy.
+            for sd in mdg_final.subdomains():
+                bg = mdg_final.subdomain_to_boundary_grid(sd)
+                bg.compute_geometry()
+
+        # Finally, we have our mdg
+        self.mdg = mdg_final
+
+        # Dimensionality of highest-dimensional manifold
+        self.nd: int = self.mdg.dim_max()
+
+        # Create projections between local and global coordinates for fracture grids.
+        pp.set_local_coordinate_projections(self.mdg)
