f5a67b102df4be071c3ef64ab32cb18c26adb7e1
Jhabriel Varela <jhabriel@gmail.com>
2025-08-18 17:16:40 -0300

    WIP: True errors for 3d verification

--- src/mdnme/examples/varela_jnum_3d/true_errors.py
+++ src/mdnme/examples/varela_jnum_3d/true_errors.py
@@ -16,13 +16,13 @@ from mdnme.examples.varela_jnum_3d.exact_solution import VarelaJNumExactSolution
 
 class VarelaJNumTrueErrors3D(VarelaJNumExactSolution3D):
     """
-    Class containing the computation of the true errors of the first numerical example.
+    Class containing the computation of true errors for the 3D manufactured solution.
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, model):
+        super().__init__(model)
 
-    def true_error(self, mdg: pp.MixedDimensionalGrid) -> float:
+    def true_error(self) -> float:
         """
         Compute global true error.
 
@@ -34,6 +34,7 @@ class VarelaJNumTrueErrors3D(VarelaJNumExactSolution3D):
             Value of the true error for the whole mixed-dimensional grid.
 
         """
+        mdg = self.model.mdg
         sd_2d, d_2d = mdg.subdomains(return_data=True, dim=2)[0]
         sd_1d, d_1d = mdg.subdomains(return_data=True, dim=1)[0]
         intf, d_intf = mdg.interfaces(return_data=True)[0]
@@ -53,146 +54,136 @@ class VarelaJNumTrueErrors3D(VarelaJNumExactSolution3D):
 
         return true_error
 
-    def true_error_matrix(self, sd_matrix: pp.Grid, d_matrix: dict) -> np.ndarray:
+    def true_error_matrix(self) -> np.ndarray:
         """
         Compute true error contribution of the matrix subdomain.
-
-        Parameters:
-            sd_matrix: pp.Grid
-                Matrix subdomain grid.
-            d_matrix: dict
-                Data dictionary of the matrix. We assume that the numerical pressure
-                has been reconstructed, i.e., that d["estimates"][
-                "recon_sd_pressure"] is available.
 
         Returns:
             True error contribution of the matrix, containing the local error squared
             of each element of the grid. Shape is `sd.num_cells`.
 
+         Note:
+             - We assume the field ``d_matrix["estimates"]["recon_sd_pressure"]`` is
+               available.
+             - We employ a numerical quadrature scheme that is accurate up to a
+               polynomial degree 10.
+
         """
-        # ---> Get list of cell indices
-        cc = sd_matrix.cell_centers
-        bot = cc[1] < 0.25
-        mid = (cc[1] >= 0.25) & (cc[1] <= 0.75)
-        top = cc[1] > 0.75
-        cell_idx = [bot, mid, top]
+        # Retrieve matrix grid and its dictionary
+        mdg: pp.MixedDimensionalGrid = self.model.mdg
+        sd_matrix, d_matrix = mdg.subdomains(return_data=True)[0]
 
-        # ---> Obtain exact pressure gradient
-        x, y = sym.symbols("x y")
+        # Retrieve indices
+        cell_idx = self.get_region_indices(where="cc")
 
-        grad_p_matrix_sym = [[sym.diff(p, x), sym.diff(p, y)] for p in self.p_matrix]
+        # Manually obtain the exact pressure gradient
+        x, y, z = sym.symbols("x y z")
 
+        # Pressure gradient as a list of symbolic variables
+        grad_p_matrix_sym = [
+            [sym.diff(p, x), sym.diff(p, y), sym.diff(p, z)]
+            for p in self.p_matrix
+        ]
+
+        # Lambdified list of pressure gradients
         grad_p_matrix_fun = [
             [
-                sym.lambdify((x, y), gradp[0], "numpy"),
-                sym.lambdify((x, y), gradp[1], "numpy"),
+                sym.lambdify((x, y, z), gradp[0], "numpy"),
+                sym.lambdify((x, y, z), gradp[1], "numpy"),
+                sym.lambdify((x, y, z), gradp[2], "numpy"),
             ]
             for gradp in grad_p_matrix_sym
         ]
 
-        # ---> Obtain reconstructed pressure and create list of coefficients
-        recon_p = d_matrix["estimates"]["recon_sd_pressure"]
+        # Obtain reconstructed pressure and create list of coefficients
+        recon_p = d_matrix["estimates"]["recon_sd_pressure"].copy()
         pr = mdnme.utils.poly2col(recon_p)
 
-        # ---> Obtain elements and declare integration method
+        # Obtain elements and declare integration method
         method = quadpy.t2.get_good_scheme(10)
         elements = mdnme.utils.get_quadpy_elements(sd_matrix)
 
-        # ---> Compute the true error for each subregion
+        # Compute the true error for each subregion
         integral = np.zeros(sd_matrix.num_cells)
         for gradp, idx in zip(grad_p_matrix_fun, cell_idx):
             # Declare integrand and add subregion contribution
             def integrand(x):
-                # Exact pressure gradient in x and y
-                gradp_exact_x = gradp[0](x[0], x[1])
-                gradp_exact_y = gradp[1](x[0], x[1])
-                # Reconstructed pressure gradient in x and y
+                # Exact pressure gradient in x, y and z
+                gradp_exact_x = gradp[0](x[0], x[1], x[2])
+                gradp_exact_y = gradp[1](x[0], x[1], x[2])
+                gradp_exact_z = gradp[2](x[0], x[1], x[2])
+                # Reconstructed pressure gradient in x, y and z
                 gradp_recon_x = pr[0] * np.ones_like(x[0])
-                gradp_recon_y = pr[1] * np.ones_like(x[1])
-                # Integral in x and y
+                gradp_recon_y = pr[1] * np.ones_like(x[0])
+                gradp_recon_z = pr[2] * np.ones_like(x[0])
+                # Integral in x, y and z
                 int_x = (gradp_exact_x - gradp_recon_x) ** 2
                 int_y = (gradp_exact_y - gradp_recon_y) ** 2
-                return int_x + int_y
+                int_z = (gradp_exact_z - gradp_recon_z) ** 2
+                return int_x + int_y + int_z
 
             integral += method.integrate(integrand, elements) * idx
 
         return integral
 
-    def true_error_fracture(self, sd_fracture: pp.Grid, d_fracture: dict) -> np.ndarray:
-        """
-        Compute true error contribution of the fracture subdomain.
-
-        Parameters:
-            sd_fracture: pp.Grid
-                Fracture subdomain grid.
-            d_fracture: dict
-                Data dictionary of the fracture. We assume that the numerical pressure
-                has been reconstructed, i.e., that d["estimates"][
-                "recon_sd_pressure"] is available.
+    def true_error_fracture(self) -> np.ndarray:
+        """Compute true error contribution of the fracture subdomain.
 
         Returns:
             True error contribution of the fracture, containing the local error squared
             of each element of the grid. Shape is `sd.num_cells`.
 
-        """
-        # ---> Obtain exact pressure gradient
-        y = sym.symbols("y")
-        grad_p_frac_sym = sym.diff(self.p_frac, y)
-        grad_p_frac_fun = sym.lambdify(y, grad_p_frac_sym, "numpy")
+        Note:
+            - We assume the field ``d_matrix["estimates"]["recon_sd_pressure"]`` is
+              available.
+            - We employ a numerical quadrature scheme that is accurate up to a
+              polynomial degree 10.
 
-        # ---> Get hold of reconstructed pressure and create list of coefficients
-        recon_p = d_fracture["estimates"]["recon_sd_pressure"]
+        """
+        mdg = self.model.mdg
+        sd_frac, d_frac = mdg.subdomains(return_data=True)[1]
+        sd_rot = mdnme.RotatedGrid(sd_frac)
+
+        # Declare symbolic variables
+        y, z = sym.symbols("y z")
+
+        # Compute symbolic and lambdified pressure gradient
+        grad_p_frac_sym = [
+            sym.diff(self.p_frac, y),
+            sym.diff(self.p_frac, z)
+        ]
+        grad_p_frac_fun = [
+            sym.lambdify((y, z), gradp, "numpy") for gradp in grad_p_frac_sym
+        ]
+
+        # Get hold of reconstructed pressure and create list of coefficients
+        recon_p = d_frac["estimates"]["recon_sd_pressure"].copy()
         pr = mdnme.utils.poly2col(recon_p)
 
-        # ---> Obtain elements and declare integration method
-        method = quadpy.c1.newton_cotes_closed(10)
-        elements = mdnme.utils.get_quadpy_elements(sd_fracture)
-        elements *= -1  # we have to use the real `y` coordinates here
+        # Obtain elements and declare integration method
+        method = quadpy.t2.get_good_scheme(10)
+        elements = mdnme.utils.get_quadpy_elements(sd_frac, sd_rot)
+        elements *= -1  # we have to use the physical coordinates here
 
-        # ---> Compute true error
+        # Compute true error
         def integrand(x):
             # Exact pressure gradient
-            gradp_exact_rot = -grad_p_frac_fun(x)  # -1 due to rotation
-            # Reconstructed pressure gradient
-            gradp_recon_x = pr[0] * np.ones_like(x[0])
-            # Intregral
-            int_x = (gradp_exact_rot - gradp_recon_x) ** 2
-            return int_x
+            gradp_exact_rot_y = -grad_p_frac_fun[0](x[0], x[1])  # -1 due to rotation
+            gradp_exact_rot_z = -grad_p_frac_fun[1](x[0], x[1])  # -1 due to rotation
+            # Reconstructed pressure gradients
+            gradp_recon_y = pr[1] * np.ones_like(x[0])
+            gradp_recon_z = pr[2] * np.ones_like(x[0])
+            # Integral
+            int_y = (gradp_exact_rot_y - gradp_recon_y) ** 2
+            int_z = (gradp_exact_rot_z - gradp_recon_z) ** 2
+            return int_y + int_z
 
         integral = method.integrate(integrand, elements)
 
         return integral
 
-    def true_error_interface(
-        self,
-        intf: pp.MortarGrid,
-        data_intf: dict,
-        sd_high: pp.Grid,
-        data_high: dict,
-        sd_low: pp.Grid,
-        data_low: dict,
-    ) -> np.ndarray:
-        """
-        Compute true error contribution of the interface.
-
-        Parameters:
-            intf: pp.MortarGrid
-                Interface (mortar) grid.
-            data_intf: dict
-                Data dictionary associated to the interface grid. Not really used
-                here, but included for consistency.
-            sd_high: pp.Grid
-                Matrix subdomain grid.
-            data_high: dict
-                Data dictionary of the Matrix. We assume that the numerical pressure
-                has been reconstructed, i.e., that d["estimates"][
-                "recon_sd_pressure"] is available.
-            sd_low: pp.Grid
-                Fracture subdomain grid.
-            data_low: dict
-                Data dictionary of the fracture. We assume that the numerical pressure
-                has been reconstructed, i.e., that d["estimates"][
-                "recon_sd_pressure"] is available.
+    def true_error_interface(self) -> np.ndarray:
+        """Compute true error contribution of the interface.
 
         Returns:
             True error contribution of the interface, containing the local error squared
@@ -204,6 +195,12 @@ class VarelaJNumTrueErrors3D(VarelaJNumExactSolution3D):
             _get_high_pressure_trace,
             _get_low_pressure,
         )
+
+        # Retrieve grids and dictionaries
+        mdg: pp.MixedDimensionalGrid = self.model.mdg
+        sd_high, data_high = mdg.subdomains(return_data=True)[0]
+        sd_low, data_low = mdg.subdomains(return_data=True)[1]
+        intf, data_intf = mdg.interfaces(return_data=True)[0]
 
         # Face-cell map between higher- and lower-dimensional subdomains
         frac_faces = sps.find(intf.primary_to_mortar_avg())[1]
@@ -229,24 +226,24 @@ class VarelaJNumTrueErrors3D(VarelaJNumExactSolution3D):
             sidegrid = side[1]
 
             # Declare integration method
-            method = quadpy.c1.newton_cotes_closed(10)
-            elements = mdnme.utils.get_quadpy_elements(sidegrid)
+            method = quadpy.t2.get_good_scheme(10)
+            sidegrid_rot = mdnme.RotatedGrid(sidegrid)
+            elements = mdnme.utils.get_quadpy_elements(sidegrid, sidegrid_rot)
             elements *= -1  # We need to use real coordinates
 
             # Project relevant quantities to the side grid
             recon_deltap_side = projector * recon_deltap
 
-            # Retrieve exact 1d pressure
-            y = sym.symbols("y")
-            exact_p_1d = sym.lambdify(y, self.p_frac, "numpy")
-            exact_deltap_side = exact_p_1d  # high-dim pressure trace is zero
+            # Retrieve exact 2D pressure
+            y, z = sym.symbols("y z")
+            exact_p_2d = sym.lambdify((y, z), self.p_frac, "numpy")
+            exact_deltap_side = exact_p_2d  # high-dim pressure trace is zero
 
             # Declare integrand
             def integrand(x):
-                coors = x[np.newaxis, :, :]  # this is needed for 1D grids
                 recon_p_jump = mdnme.utils.evaluate_p1(
                     recon_deltap_side,
-                    -coors,  # negate due to rotation
+                    -x,  # negate due to rotation
                 )
                 return (exact_deltap_side(x) - recon_p_jump) ** 2
 
