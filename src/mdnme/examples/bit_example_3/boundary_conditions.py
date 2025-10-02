e48a7ea93bae3306be10df1db8a96e529722cab9
Jhabriel Varela <jhabriel@gmail.com>
2025-10-01 22:33:14 -0300

    Add Example 3 model and analysis scripts
    Introduces the Example 3 model, modified boundary conditions, and analysis runner for the benchmark. Includes model definition, boundary condition logic, and a script to run simulations, estimate errors, and export results.

---
+++ src/mdnme/examples/bit_example_3/boundary_conditions.py
@@ -0,0 +1,51 @@
+"""Module containing the modified boundary conditions mixing.
+
+The original benchmark imposes an inlet constant Neumann flux which we have to
+replace by a constant pressure boundary condition.
+
+"""
+
+import porepy as pp
+import numpy as np
+
+
+class BoundaryConditionsModified(pp.PorePyModel):
+    """Define inlet and outlet boundary conditions."""
+
+    def bc_type_darcy_flux(self, sd: pp.Grid) -> pp.BoundaryCondition:
+        """Assign Dirichlet to the top and bottom  part of the north (y=y_max)
+        boundary as well as the middle portion of the south (y=y_min)."""
+        # Retrieve boundary sides.
+        domain_sides = self.domain_boundary_sides(sd)
+        # Get Dirichlet faces.
+        dir_faces = np.zeros(sd.num_faces, dtype=bool)
+        # top purple region from the original paper
+        north_top_dir_cells = sd.face_centers[2][domain_sides.north] > (2 / 3)
+        # bottom purple region from the original paper
+        north_bottom_dir_faces = sd.face_centers[2][domain_sides.north] < (1 / 3)
+        # blue region from the original paper
+        south_middle_dif_faces = \
+            (sd.face_centers[2][domain_sides.south] < (2 / 3)) \
+            & (sd.face_centers[2][domain_sides.south] > (1 / 3))
+        # Set north faces bc-types
+        dir_faces[domain_sides.north] = north_top_dir_cells + north_bottom_dir_faces
+        # Set south faces bc-types
+        dir_faces[domain_sides.south] = south_middle_dif_faces
+        # Assign boundary conditions, the rest are Neumann by default.
+        bc = pp.BoundaryCondition(sd, dir_faces, "dir")
+        return bc
+
+    def bc_values_darcy_flux(self, bg: pp.BoundaryGrid) -> np.ndarray:
+        """Assign non-zero Darcy flux to the middle south (y=y_min) boundary."""
+        # Retrieve boundary sides and cell centers.
+        domain_sides = self.domain_boundary_sides(bg)
+        cc = bg.cell_centers
+        # Get inlet faces.
+        inlet_faces = np.zeros(bg.num_cells, dtype=bool)
+        inlet_faces[domain_sides.south] = (cc[2][domain_sides.south] < (2 / 3)) & (
+            cc[2][domain_sides.south] > (1 / 3)
+        )
+        # Assign unitary pressure in the inlet boundary (blue region)
+        values = np.zeros(bg.num_cells)
+        values[inlet_faces] = 1
+        return values
---
+++ src/mdnme/examples/bit_example_3/model.py
@@ -0,0 +1,72 @@
+"""
+This module contains the model for the Example 3 of the manuscript.
+"""
+import sys
+
+from typing import Callable
+
+import porepy as pp
+import mdnme
+
+from mdnme.estimates.helpers import ErrorEstimatesSaveData
+
+from porepy.examples.flow_benchmark_3d_case_3 import (
+    FlowBenchmark3dCase3Model,
+    solid_constants,
+)
+from mdnme.examples.bit_example_3.boundary_conditions import BoundaryConditionsModified
+
+
+class SmallFeaturesSolutionStrategy(
+    pp.fluid_mass_balance.SolutionStrategySinglePhaseFlow
+):
+    """Modified solution strategy for the verification setup."""
+
+    mdg: pp.MixedDimensionalGrid
+    """Mixed-dimensional grid."""
+
+    fluid: pp.Fluid
+    """Object containing the fluid constants."""
+
+    solid: pp.SolidConstants
+    """Object containing the solid constants."""
+
+    error_estimates_data_saving: Callable
+    """Method to save solution data to be used in a posteriori error estimation."""
+
+    def __init__(self, params: dict):
+        """Constructor for the class."""
+
+        super().__init__(params)
+
+    def after_simulation(self) -> None:
+        """Method to be called after the simulation has finished."""
+        # Save error estimates data
+        self.error_estimates_data_saving()
+
+    def _is_nonlinear_problem(self) -> bool:
+        """The problem is linear."""
+        return False
+
+    def _is_time_dependent(self) -> bool:
+        """The problem is stationary."""
+        return False
+
+
+# %% Mixer
+class SmallFeaturesModel(  # type: ignore[misc]
+    ErrorEstimatesSaveData,
+    SmallFeaturesSolutionStrategy,
+    BoundaryConditionsModified,
+    FlowBenchmark3dCase3Model,
+):
+    """Main model for running the analysis corresponding to example number 3."""
+
+
+# %% Runner
+params = {
+    "material_constants": {"solid": solid_constants},
+    "refinement_level": 0,
+}
+model = SmallFeaturesModel(params)
+pp.run_time_dependent_model(model, params)
