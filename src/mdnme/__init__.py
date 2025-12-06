9cfbb16fdb324319d013a4537df91d09e9f48133
Jhabriel Varela <jhabriel@gmail.com>
2025-12-06 15:50:37 -0300

    STY: `isort` once again

--- src/mdnme/__init__.py
+++ src/mdnme/__init__.py
@@ -1,16 +1,14 @@
 # Error estimation
+# Utilities
+from mdnme import utils
 from mdnme.estimates.diffusive_error import compute_diffusive_error
-from mdnme.estimates.residual_error import compute_residual_error
 from mdnme.estimates.error_estimation import (
     compute_error_indicators,
     estimate_errors,
     get_majorant,
 )
-
 from mdnme.estimates.flux_extension import extend_fv_fluxes
-from mdnme.estimates.pressure_reconstruction import reconstruct_pressure
 from mdnme.estimates.helpers import ErrorEstimatesSaveData
-
-# Utilities
-from mdnme import utils
-from mdnme.utils.grid_rotation import RotatedGrid, rotate_grid, canonical_frame
+from mdnme.estimates.pressure_reconstruction import reconstruct_pressure
+from mdnme.estimates.residual_error import compute_residual_error
+from mdnme.utils.grid_rotation import RotatedGrid, canonical_frame, rotate_grid
